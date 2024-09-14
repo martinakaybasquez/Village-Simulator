f4d317b43a7578054e42221b7326b615ee837a03
Martina Kay Basquez <martinabasquez@gmail.com>
2024-09-13 20:51:02 -0500

    set up skeleton of program / added resource array to village service

---
+++ Simulator/src/app/components/map/edit-improvement-dialog/edit-improvement-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-improvement-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-improvement-dialog.component.html',
+  styleUrl: './edit-improvement-dialog.component.css'
+})
+export class EditImprovementDialogComponent {
+
+}
--- Simulator/src/app/services/village.service.ts
+++ Simulator/src/app/services/village.service.ts
@@ -5,12 +5,57 @@ import { ImprovementModel } from '../models/improvement-model';
   providedIn: 'root'
 })
 export class VillageService {
-
+  
   constructor() { }
   
   improvement:ImprovementModel[] = [];
-  allImprovements = 
-    {house: { cost: { people: 0, lumber: 5, grain: 5, water: 5, sheep: 1 }, resource: { people: 5, lumber: 0, grain: 0, water: 0, sheep: 0 }}}
+  //allImprovements = current resource totals 
+  allImprovements:ImprovementModel[] = [
+    {
+      type:"house",
+      level:1,
+      cost:{lumber:5, grain:5, water:5, sheep:1, person:0},
+      resource:{lumber:0, grain:0, water:0, sheep:0, person:5}
+    },
+    {
+      type:"field",
+      level:1,
+      cost:{lumber:0, grain:0, water:2, sheep:0, person:1},
+      resource:{lumber:0, grain:10, water:0, sheep:0, person:0}
+    },
+    {
+      type:"pasture",
+      level:1,
+      cost:{lumber:0, grain:2, water:2, sheep:0, person:1},
+      resource:{lumber:0, grain:0, water:0, sheep:5, person:0}
+    },
+    {
+      type:"lumber mill",
+      level:1,
+      cost:{lumber:0, grain:0, water:0, sheep:0, person:1},
+      resource:{lumber:10, grain:0, water:0, sheep:0, person:0}
+    },
+    {
+      type:"well",
+      level:1,
+      cost:{lumber:2, grain:0, water:0, sheep:0, person:1},
+      resource:{lumber:0, grain:0, water:10, sheep:0, person:0}
+    }]
   
+  addImprovement(){
+    
+  }
+
+  removeImprovement(){
+
+  }
+
+  upgradeImprovement(){
+
+  }
+
+  downgradeImprovement(){
+
+  }
 }
 
